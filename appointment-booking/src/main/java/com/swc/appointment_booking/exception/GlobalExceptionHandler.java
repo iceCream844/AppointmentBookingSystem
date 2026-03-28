f4b66e93264347f138fb8233cb59715859ef7712
SanWC <shengou0409@gmail.com>
2026-03-29 03:50:22 +0800

    Add AccessDeniedException for JWT

--- appointment-booking/src/main/java/com/swc/appointment_booking/exception/GlobalExceptionHandler.java
+++ appointment-booking/src/main/java/com/swc/appointment_booking/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.swc.appointment_booking.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -25,6 +26,14 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
                 .body("Something went wrong");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<String> handleAccessDenied(AccessDeniedException ex) {
+
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .body("Access denied");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
