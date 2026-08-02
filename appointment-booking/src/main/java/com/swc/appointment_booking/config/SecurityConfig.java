675fb6ffb91c9064c3a994baa0c42f61fb768b6e
SanWC <shengou0409@gmail.com>
2026-08-02 20:21:03 +0800

    added swagger

--- appointment-booking/src/main/java/com/swc/appointment_booking/config/SecurityConfig.java
+++ appointment-booking/src/main/java/com/swc/appointment_booking/config/SecurityConfig.java
@@ -29,6 +29,13 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .requestMatchers("/api/auth/**").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/users").permitAll()
+                        // Swagger
+                        .requestMatchers(
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(jwtAuthenticationFilter,
